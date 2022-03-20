a2568e1787c7b371a125123dd345e627540914b4
pavemen3 <43461016+pavemen3@users.noreply.github.com>
2022-03-20 00:49:07 +0000

    fontawesome 表示不具合修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-
+    @user = User.find(current_user.id)
   end
 
   private
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,8 +12,9 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
-// import '@fortawesome/fontawesome-free/js/all'
+import '@fortawesome/fontawesome-free/js/all'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+// global.FontAwesome.config.mutateApproach = 'sync'
