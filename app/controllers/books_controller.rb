92ae656b0fbb7e2216ed62677e2747e8b9fdfe90
pavemen3 <43461016+pavemen3@users.noreply.github.com>
2022-03-21 11:02:35 +0000

    [finish] 課題：アプリケーションを作成してみよう：応用編

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,8 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@new_book.id)
     else
+      @books = Book.all
+      @user = User.find(current_user.id)
       render :index
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # }
   devise_for :users
   root to: 'homes#top'
-  get "homes/about" ,to: 'homes#about', as: "about"
+  get "home/about" ,to: 'homes#about', as: "about"
   resources :users, only: [:index, :show, :edit, :update, :destroy]
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
