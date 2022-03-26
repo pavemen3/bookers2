3188fc5713ffe4d1b8c185e4f21c2ca97424ff41
pavemen3 <43461016+pavemen3@users.noreply.github.com>
2022-03-26 15:26:09 +0000

    ゲストログイン機能追加とRubocopインストール

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :correct_user, only: [:edit, :update]
+  before_action :ensure_guest_user, only: [:edit]
 
   def index
     @users = User.all
@@ -38,4 +39,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(user_path(current_user.id)) unless @user == current_user
   end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,9 @@ Rails.application.routes.draw do
   get "home/about" ,to: 'homes#about', as: "about"
   resources :users, only: [:index, :show, :edit, :update, :destroy]
   resources :books
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
